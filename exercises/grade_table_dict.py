3440dd46f41569daf6cf8d1fade94a0ee0ca9780
kwonb <Bryant.Kwon@adp.com>
2018-01-03 22:37:40 -0800

    initial commit

---
+++ exercises/grade_table_dict.py
@@ -0,0 +1,32 @@
+"""
+Create a dictionary grade_table whose keys are provided
+student names and values are associated list of grades
+"""
+
+
+# Add code here
+# {"key" : value}
+grade_table = {"Bryant" : [100, 98, 100, 13],
+               "Aiden" : [75, 59, 89, 77],
+               "Emma" : [86, 84, 91, 78]}
+
+fav_food_table = {"Bryant" : "Pizza",
+                  "Aiden": "Hamburger",
+                  "Emma": "Milk"}
+
+# Tests
+print(grade_table["Bryant"])
+print(grade_table["Aiden"])
+print(grade_table["Emma"])
+
+print(fav_food_table["Bryant"])
+print(fav_food_table["Aiden"])
+print(fav_food_table["Emma"])
+
+
+# Output
+#[100, 98, 100, 13]
+#[75, 59, 89, 77]
+#[86, 84, 91, 78]
+
+
